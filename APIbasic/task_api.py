740c7d462ffd7b90ffa1f49c7fca844c26d27430
Rishu <500061292@upesalumni.upes.ac.in>
2023-09-11 00:58:57 +0530

    Debugged the api for HTTP request types

--- APIbasic/task_api.py
+++ APIbasic/task_api.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Sample data for demo
-data = {'item' : [{'id':1, 'name': 'Item 1'}, {'id':2, 'name':'Item2'}]}
+data = {'items' : [{'id':1, 'name': 'Item 1'}, {'id':2, 'name':'Item2'}]}
 
 # Route to get a list of items
 @app.route('/api/items', methods=['GET'])
@@ -11,7 +11,7 @@ def get_items():
     return jsonify(data)
 
 # Route to get a specific item by ID
-@app.route('/api/item/<int:item_id>', methods=['GET'])
+@app.route('/api/items/<int:item_id>', methods=['GET'])
 def get_item(item_id):
     item = next((item for item in data['items'] if item['id'] == item_id), None)
     if item:
@@ -21,7 +21,7 @@ def get_item(item_id):
 # Route to add a new item 
 @app.route('/app/items', methods=['POST'])
 def add_item():
-    new_item = request.get.json()
+    new_item = request.get_json()
     if 'name' in new_item:
         new_item['id'] = len(data['items']) + 1
         data['items'].append(new_item)
@@ -31,20 +31,23 @@ def add_item():
 # Route to update an existing item
 @app.route('/api/items/<int:item_id>', methods=['PUT'])
 def update_item(item_id):
-    updated_item = request.get_jason()
-    item = next((item for item in data['item'] if item['id'] == item_id), None)
-    if item:
-        item['name'] = updated_item.get('name', item['name'])
-        return jsonify(item)
-    return jsonify({'message:' 'Item not found'}), 404
+    updated_item = request.get_json()
+    items = data.get('items', [])
+
+    for item in items:
+        if item['id'] == item_id:
+            item['name'] = updated_item.get('name', item['name'])
+            return jsonify(item), 200
+    return jsonify({'message': 'Item not found'}), 404    
 
 # Route to delete an item by ID 
 @app.route('/api/items/<int:item_id>', methods=['DELETE'])
 def delete_item(item_id):
-    item = next((item for item in data['item'] if item['id'] == item_id), None)
+    items = data.get('items', [])
+    item = next((item for item in items if item['id'] == item_id), None)
     if item:
-        data['items'].remove(item)
-        return jsonify({'message': 'Item deleted'})
+        items.remove(item)
+        return jsonify({'message': 'Item deleted'}), 200
     return jsonify({'message': 'Item not found'}), 404
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # API
 Playground for API dev.
+
+################################################
+Curl Commands to access the PUT, POST and DELETE 
+-X : Specifies the HTTP request type 
+-H : Header type 
+-d : data 
+
+For "POST" :
+    curl -X POST -H "Content-Type: application/json" -d '{"name": "Item 4"}' http://localhost/...
+For "PUT" :
+    curl -X PUT -H "Content-Type: application/json" -d '{"name": "Updated Item"}' http://localhost:5000/api/items/1
+For "DELETE" :
+    curl -X DELETE -H "Content-Type: application/json" -d '{"name": "Updated Item"}' http://localhost/api/items/1
+#################################################
